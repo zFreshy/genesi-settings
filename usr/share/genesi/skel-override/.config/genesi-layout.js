89ac78e2f779fec8b4a8328a9809e6361779e048
zFreshy <matheusvinni109@gmail.com>
2026-05-06 19:56:08 -0300

    style: revert to single bottom floating taskbar

--- usr/share/genesi/skel-override/.config/genesi-layout.js
+++ usr/share/genesi/skel-override/.config/genesi-layout.js
@@ -3,39 +3,35 @@ for (var i = 0; i < p.length; i++) {
     p[i].remove();
 }
 
-var topPanel = new Panel("org.kde.panel");
-topPanel.location = "top";
-topPanel.height = 32;
-topPanel.alignment = "left";
-topPanel.opacity = "translucent";
-
-topPanel.addWidget("org.kde.plasma.appmenu");
-topPanel.addWidget("org.kde.plasma.panelspacer");
-var clock = topPanel.addWidget("org.kde.plasma.digitalclock");
-clock.currentConfigGroup = ["Appearance"];
-clock.writeConfig("dateFormat", "isoDate");
-clock.writeConfig("use24hFormat", "2");
-topPanel.addWidget("org.kde.plasma.panelspacer");
-topPanel.addWidget("org.kde.plasma.systemtray");
-
 var bottomDock = new Panel("org.kde.panel");
 bottomDock.location = "bottom";
-bottomDock.height = 56;
+bottomDock.height = 50;
 bottomDock.alignment = "center";
-bottomDock.lengthMode = "fit";
 bottomDock.floating = true;
-bottomDock.opacity = "translucent";
 
 var launcher = bottomDock.addWidget("org.kde.plasma.kicker");
 launcher.currentConfigGroup = ["General"];
 launcher.writeConfig("icon", "/usr/share/pixmaps/genesi-logo.png");
 
+bottomDock.addWidget("org.kde.plasma.panelspacer");
+
 var tasks = bottomDock.addWidget("org.kde.plasma.icontasks");
 tasks.currentConfigGroup = ["General"];
+tasks.writeConfig("iconSpacing", "2");
 tasks.writeConfig("launchers", "applications:org.kde.dolphin.desktop,preferred://browser,applications:org.kde.konsole.desktop,applications:systemsettings.desktop");
+
+bottomDock.addWidget("org.kde.plasma.panelspacer");
 
 var aiBtn = bottomDock.addWidget("org.kde.plasma.icon");
 aiBtn.currentConfigGroup = ["General"];
 aiBtn.writeConfig("applicationName", "Genesi AI Mode");
 aiBtn.writeConfig("iconName", "cpu");
 aiBtn.writeConfig("url", "file:///usr/share/applications/genesi-aimode.desktop");
+
+var tray = bottomDock.addWidget("org.kde.plasma.systemtray");
+var clock = bottomDock.addWidget("org.kde.plasma.digitalclock");
+clock.currentConfigGroup = ["Appearance"];
+clock.writeConfig("dateFormat", "isoDate");
+clock.writeConfig("use24hFormat", "2");
+
+bottomDock.addWidget("org.kde.plasma.showdesktop");
